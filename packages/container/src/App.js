7450da95a4da5a972e4d91310dd1a063a06ebdd6
Katya <katya.p.1129@gmail.com>
2023-07-17 21:02:53 -0400

    update container header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
   return (
     <div>
-      <h1>Hi HOLAA</h1>
+      <h1>Hi FROM THE CONTAINER</h1>
       <hr />
       <MarketingApp />
     </div>
