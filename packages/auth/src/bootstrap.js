b074930f90561fba52cd6d6be911c47b75e3c7f2
Katya <katya.p.1129@gmail.com>
2023-07-20 15:52:26 -0400

    set publicPath for webpack in dev, add auth

---
+++ packages/auth/src/bootstrap.js
@@ -0,0 +1,40 @@
+// main startup code
+import React from "react";
+import ReactDOM from "react-dom";
+import { createMemoryHistory, createBrowserHistory } from "history";
+import App from "./App";
+
+// mount function to start up the app
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(<App history={history} />, el);
+
+  // add return with an object of functions to allow
+  // the container app in production to communicate with
+  // the auth app
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location;
+
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    },
+  };
+};
+
+// if in development and in isolation, mount immediately
+if (process.env.NODE_ENV === "development") {
+  const devRoot = document.querySelector("#_auth-dev-root");
+  if (devRoot) {
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
+  }
+}
+
+// if running through Container, just export the mount function
+export { mount };
--- packages/auth/src/index.js
+++ packages/auth/src/index.js
@@ -1,2 +1,4 @@
 // gives the browser time to load resources by importing dynamically
 import("./bootstrap");
+
+console.log("anyone home?");
